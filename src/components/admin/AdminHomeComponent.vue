5ce93b5470843b7ff91f8a9aceac9034d5db56c5
Lamarck <704021302@qq.com>
2024-12-25 10:52:20 +0800

    完善了SuperAdminHome组件的部分内容

--- src/components/admin/AdminHomeComponent.vue
+++ src/components/admin/AdminHomeComponent.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
     <span>This is AdminHome</span>
+    <h1>我是标题</h1>
+    <h2>我是小标题</h2>
+    <button @click="sayHello">我是一个按钮</button>
   </div>
 </template>
 
-<script setup>
+<script>
+export default{
+  name: "AdminHomeComponent",
+  data(){
 
+  },
+  methods:{
+    sayHello(){
+      alert("You are a pig")
+    }
+  }
+}
 </script>
 
 <style scoped>
--- src/components/superAdmin/SuperAdminHomeComponent.vue
+++ src/components/superAdmin/SuperAdminHomeComponent.vue
@@ -2,13 +2,14 @@
   <!--Aside-->
   <el-container style="height: 100%; border: 1px solid #eee; margin-top: -9px">  <!--整个页面中最大的容器-->
     <!--Aside部分-->
-    <el-aside style="height: 100vh; width: 200px; background-color: rgb(238, 241, 246); margin-left: -9px">  <!--页面中的左侧导航栏-->
+    <el-aside :width="aside_width" style="height: 100vh; background-color: rgb(238, 241, 246); margin-left: -9px">  <!--页面中的左侧导航栏-->
       <el-menu
           background-color="#545c64"
           text-color="#fff"
           active-text-color="#ffd04b"
           default-active="/Home"
           style="height: 100%"
+          :collapse="isCollapse"
           :collapse-transition="false"
           router
       >
@@ -33,11 +34,11 @@
       <!--Header部分-->
       <el-header style="text-align: right; font-size: 12px; border-bottom: darkgray 3px solid">  <!--页面右侧的header-->
         <div style="display: flex;line-height: 60px">
-          <div>
-            <i class="el-icon-s-fold" style="font-size: 25px; cursor: pointer"></i>  <!--给侧边栏控制按钮添加了点击事件，点击就会触发collapse方法-->
+          <div >
+            <i :class="collapseIcon" style="font-size: 25px; cursor: pointer" @click="collapse"></i>  <!--给侧边栏控制按钮添加了点击事件，点击就会触发collapse方法-->
           </div>
           <div style="flex:1; text-align: center; font-size: 26px">
-            <span>Welcome to Lamarck's Warehouse Management System</span>
+            <span>Welcome to RabGenDB</span>
           </div>
           <el-dropdown>
             <i class="el-icon-user-solid" style="font-size: 25px"></i>
@@ -70,9 +71,25 @@ export default {
   components: {},
   data(){
     return{
+      aside_width: '200px',
+      isCollapse: false,
+      collapseIcon: 'el-icon-s-fold'
     }
   },
   props:{
+  },
+  methods:{
+    collapse(){
+      this.isCollapse = !this.isCollapse;
+      if(this.isCollapse){
+        this.aside_width = '64px';
+        this.collapseIcon = 'el-icon-s-unfold';
+      }
+      else{
+        this.aside_width = '200px';
+        this.collapseIcon = 'el-icon-s-fold';
+      }
+    }
   }
 }
 </script>
